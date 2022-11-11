41ef6957781f1802dee59e30defc569269acec14
unknown <dvtorushin105@gmail.com>
2022-11-12 00:58:39 +0700

    first commit

---
+++ up2-dart/bin/up2_dart.dart
@@ -0,0 +1,5 @@
+import 'package:up2_dart/up2_dart.dart' as up2_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${up2_dart.calculate()}!');
+}
---
+++ up2-dart/test/up2_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:up2_dart/up2_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
